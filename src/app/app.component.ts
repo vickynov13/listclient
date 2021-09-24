7687f214ac1edaab0100a18fec536b1ebbb3ac9f
vickynov13 <52153155+vickynov13@users.noreply.github.com>
2021-09-25 00:30:36 +0530

    Fixed Nav bar issues
    1. Issue : Not a known property, fixed b adding attr.aria-expanded.
    2. added reset function to close dropdown, or expanded nav bar in mobile mode on clicking anywhere in container or clicking dropdown options

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'listclient';
+  toggledrop = false;
+  dropdown = "";
+  navbutton = false;
+  buttonstat = "collapsed";
+  divstat="";
+  constructor(private router: Router){
+
+  }
+  ngOnInit(){
+    this.router.navigate(['']);
+  }
+
+  dropdowntoggle():void{
+    this.toggledrop = !this.toggledrop;
+    if(this.toggledrop){
+      this.dropdown = "show";
+    }else{
+      this.dropdown = "";
+    }
+  }
+  navbuttonclick():void{
+    this.navbutton=!this.navbutton;
+    if(this.navbutton){
+      this.buttonstat ="";
+      this.divstat="show";
+    }else{
+      this.buttonstat ="collapsed";
+      this.divstat="";
+    }
+  }
+  reset():void{
+  this.toggledrop = false;
+  this.dropdown = "";
+  this.navbutton = false;
+  this.buttonstat = "collapsed";
+  this.divstat="";
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
